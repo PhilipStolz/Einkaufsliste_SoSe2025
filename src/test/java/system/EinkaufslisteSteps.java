0e3ff1a40269584a2b4206ffd6762009bd8bffe7
p_stolz <p_stolz@hood-group.com>
2024-05-27 16:34:52 +0200

    Rümpfe in Step-Datei kopiert -> Steps pending

--- src/test/java/system/EinkaufslisteSteps.java
+++ src/test/java/system/EinkaufslisteSteps.java
@@ -15,6 +15,23 @@ public class EinkaufslisteSteps implements SchnittstelleZumKonsument {
 	public EinkaufslisteSteps() {
 		app = new EinkaufsApp(this);
 	}
+
+	@Given("eine leere Einkaufsliste")
+	public void eine_leere_einkaufsliste() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@When("ich schreibe {string} in die Einkaufsliste")
+	public void ich_schreibe_in_die_einkaufsliste(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+	@Then("die Einkaufsliste sollte den Eintrag {string} enthalten.")
+	public void die_einkaufsliste_sollte_den_eintrag_enthalten(String string) {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
 }
 
 
